cb3b9d1dfca8b0ea84ea1e9c692af6fbf9ba5100
Tanmay-r <tvrandhavane@gmail.com>
2014-02-27 23:56:10 +0530

    pthread try, creates one thread, no sync

---
+++ Assignment7/pthread.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <pthread.h>
+using namespace std;
+
+int a;
+
+void* threadCall(void* arg){
+	//cout << __LINE__ << endl;
+	a = 17;
+	cout << "a in thread = " << a << endl;
+}
+int main(){
+	cout << "a = " << a << endl;
+	pthread_t thread;
+	int retVal = pthread_create(&thread, NULL, threadCall, NULL);
+	cout << "retVal = "<< retVal << endl;
+	sleep(1);
+	a = 23;
+	cout << "a in main = " << a << endl;
+	pthread_join(thread, NULL);
+}
